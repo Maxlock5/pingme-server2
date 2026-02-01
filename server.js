c4f4dd53b56c0ac00720a57d7e2bbd71f5cae9d4
Maxlock5 <soadtg31@gmail.com>
2026-02-01 15:02:31 +0100

    Initial Railway-ready PingMe server

---
+++ server.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("PingMe WebSocket server is running");
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+const users = new Map();
+
+function broadcastUsers() {
+  const list = Array.from(users.values());
+  const msg = JSON.stringify({ type: "users", users: list });
+
+  wss.clients.forEach(c => {
+    if (c.readyState === WebSocket.OPEN) {
+      c.send(msg);
+    }
+  });
+
+  console.log("[SERVER] users:", list);
+}
+
+function removeUser(ws) {
+  const user = users.get(ws);
+  if (user) {
+    console.log("[SERVER] disconnected:", user);
+    users.delete(ws);
+    broadcastUsers();
+  }
+}
+
+server.on("upgrade", (req, socket, head) => {
+  wss.handleUpgrade(req, socket, head, ws => {
+    wss.emit("connection", ws, req);
+  });
+});
+
+wss.on("connection", ws => {
+  console.log("[SERVER] connected");
+
+  ws.on("message", data => {
+    try {
+      const msg = JSON.parse(data.toString());
+
+      if (msg.type === "register") {
+        users.set(ws, msg.userId);
+        console.log("[SERVER] registered:", msg.userId);
+        broadcastUsers();
+      }
+    } catch (e) {
+      console.error("[SERVER] parse error:", e);
+    }
+  });
+
+  ws.on("error", () => removeUser(ws));
+  ws.on("close", () => removeUser(ws));
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("[SERVER] running on port", PORT);
+});
